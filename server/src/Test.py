5f302013d37fad7686e6a84ebcb28ea61702c28a
Steven Lim <Stevenrlim@gmail.com>
2025-04-04 00:07:58 -0400

    Update Server and Python Test
    Server Logic should be finalized in this one, included send logic and finished all remaining logic (not 2,3)

--- server/src/Test.py
+++ server/src/Test.py
@@ -1,8 +1,25 @@
 import socket
+import time
 
 # Server details
 SERVER_HOST = '127.0.0.1'  # Server's IP address (localhost)
-SERVER_PORT = 1000         # Port number (make sure it matches the server's port)
+SERVER_PORT = 5006         # Port number (make sure it matches the server's port)
+
+def send_message(sock, message):
+    message = message.encode()
+    length = len(message).to_bytes(4, byteorder='big')  # 4-byte length prefix
+    sock.send(length + message)
+
+def receive_message(sock):
+    # First read the 4-byte length prefix
+    length_data = sock.recv(4)
+    if not length_data:
+        return None
+    length = int.from_bytes(length_data, byteorder='big')
+    
+    # Then read the actual message
+    data = sock.recv(length)
+    return data.decode()
 
 def main():
     # Create a TCP/IP socket
@@ -12,12 +29,25 @@ def main():
         # Connect to the server
         client_socket.connect((SERVER_HOST, SERVER_PORT))
         print(f"Connected to server at {SERVER_HOST}:{SERVER_PORT}")
+        print("Type messages to send to server. Type 'exit' to quit.")
         
-        # Send a message to the server
-        message = "Hello, Server!"
-        client_socket.sendall(message.encode())
-        print(f"Message sent: {message}")
-        
+        while True:
+            # Get user input
+            message = input("Enter message: ")
+            
+            if message.lower() == 'exit':
+                break
+                
+            # Send the message
+            send_message(client_socket, message)
+            
+            # Receive and print response
+            response = client_socket.recv(1024)
+            if response is None:
+                print("Server closed connection")
+                break
+            print(f"Server response: {response.decode()}")
+            
     except Exception as e:
         print(f"Error occurred: {e}")
     finally:
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -35,9 +35,9 @@ string get_name_from_socket(int);
 int get_socket_from_name(string);
 vector<string> splitString(const string&, char);
 void admin_send_all_message(const string&);
-int check_room(int);
+int check_room(const int&);
 void message_type_actions(const char&,int&,vector<string>&);
-
+int get_room(const int&);
 int client_count = 0;
 
 
@@ -46,7 +46,7 @@ mutex mtx;//Mutex
 condition_variable cv;
 unordered_map<string,int> client_sockets;//Map for all clients and their respective sokets
 vector<pair<int,int>> rooms(10);//10 breakout rooms with a capacity of 2 people only
-vector<int> active_sockets(10);
+vector<int> active_sockets(20);
 
 
 int main(void){
@@ -139,7 +139,7 @@ void handle_connection(int client_socket){
 
 
 
-int check_room(int room_num){
+int check_room(int &room_num){
     int count = 0;
     if(rooms[room_num-1].first == 0){
         count++;
@@ -147,7 +147,7 @@ int check_room(int room_num){
     if(rooms[room_num-1].second == 0){
         count++;
     }
-    cout << count << endl;
+    //cout << count << endl;
     return count;
 }
 
@@ -188,7 +188,6 @@ void send_message(int client_socket,const string &msg){
 void admin_send_all_message(const string &message){
     for(int i=0;i<active_sockets.size();i++){
         send_message(active_sockets[i],message);
-	
     }
 }
 
@@ -210,20 +209,45 @@ vector<string> splitString(const string& str, char delimiter){
     return result;
 }
 
+int get_room(const int& client_socket){
+   
+    for (int i = 0; i < rooms.size(); i++) {
+        if (rooms[i].first == client_socket || rooms[i].second == client_socket) {
+            return i + 1; // Room numbers start at 1
+        }
+    }	
+    return 0;
+}
+
 void message_type_actions(const char &message_type,int &client_socket,vector<string> &split_str){
         //Switch Statement
         switch(message_type) {
-            case '1'://message (receive 1;<sender>;<room_num>;<message>)
-		{ 
-			mtx.lock();	      
+            case '1'://message (receive 1;<message>)
+		{
+            int receiver;
+            int sender = client_socket;
+            mtx.lock();	      
+            int room = get_room(client_socket);
+            if(rooms[room-1].first==client_socket){
+                receiver = rooms[room-1].second;
+            }
+            else{
+                receiver = rooms[room-1].first;
+            }
+	    stringstream message;
+	    message << get_name_from_socket(client_socket) << ";" << split_str[1];
+            //sprintf(message,"%s:%s",get_name_from_socket(client_socket),split_str[1]);         
 			mtx.unlock();
+            send_message(sender,message.str());
+            send_message(receiver,message.str());
 			break;
 		}
             case '2'://New User connected to server get the nickname (receive 2;<nickname>)
 		{
 			mtx.lock();
 			add_socket_name(client_socket,split_str[1].c_str());
-			active_sockets.push_back(client_socket);
+			//active_sockets.push_back(client_socket);
+			send_message(client_socket,"Successful Connection\n");
 			printf("Client Socket ID: %d, Nickname: %s\n",client_socket,get_name_from_socket(client_socket).c_str());
 			mtx.unlock();
 			break;
@@ -234,7 +258,7 @@ void message_type_actions(const char &message_type,int &client_socket,vector<str
 			mtx.lock();
 			string nickname = get_name_from_socket(client_socket);
 			//Remove from all tracking	
-			active_sockets.erase(remove(active_sockets.begin(),active_sockets.end(),client_socket),active_sockets.end());
+			//active_sockets.erase(remove(active_sockets.begin(),active_sockets.end(),client_socket),active_sockets.end());
 			client_sockets.erase(nickname.c_str());
 			close(client_socket);
 			mtx.unlock();
@@ -242,53 +266,62 @@ void message_type_actions(const char &message_type,int &client_socket,vector<str
 			break;
 		}
             case '4': 
-		{//Join a Room (receive 4;<Room Number>;<client_nickname>) Room Number starts from 1
-			mtx.lock();
-            		int room_num = stoi(split_str[1].c_str());
-			int available_space = check_room(room_num);
-           		int client_socket_from_nickname = get_socket_from_name(split_str[2]);
-			cout << client_socket_from_nickname <<endl;
-            		if(available_space > 0){
-                		if(rooms[room_num-1].first == 0){
-                    			rooms[room_num-1].first = client_socket_from_nickname;
-                		}
-				else{
-				    int client_to_ask = rooms[room_num-1].first;
-				    //Send 9:Join Request 
-				    //9;<client_nickname>
-				    string client_nickname = get_name_from_socket(rooms[room_num-1].first);
-				    printf("Need to ask if it is ok: 9;%s\n",client_nickname.c_str());
-				    //rooms[room_num-1].second = client_socket_from_nickname;
-				}
-			 }
-			 else{
-				printf("Room is not available\n");
-			 }
-			mtx.unlock();
-			break;
-		}
-            case '5'://Accept Join Request
-		{
+		    {//Join a Room (receive 4;<Room Number>) Room Number starts from 1
+			    mtx.lock();
+           		int room_num = stoi(split_str[1].c_str());
+			    int available_space = check_room(room_num);
+           		//int client_socket_from_nickname = get_socket_from_name(split_str[2]);
+			    //cout << client_socket_from_nickname <<endl;
+                    if(available_space > 0){
+			    if(rooms[room_num-1].first ==0){
+				rooms[room_num-1].first = client_socket;
+			    }
+			    else{
+				rooms[room_num-1].second = client_socket;
+			    }
+		   // stringstream message;
+		   // message << "2;" << room_num;
+                    send_message(client_socket,"2");
+                    active_sockets.push_back(client_socket);               
+                }
+    		    else{
+				    send_message(client_socket,"3");
+			    }
+			    mtx.unlock();
+			    break;
+		    }
 
-               		break;
-		}
-            case '6'://Deny (receive <Room Number>)
-		{
-			send_message(client_socket,"6 Request to join has been denied");
-                	break;
-		}
             case '7'://Leave Room (receive 7;<Room Number>)
-		{
-			string current_nickname = get_name_from_socket(client_socket);
-		
-			break;
+		    {
+                int other;
+                //string message;
+                int room;
+			    mtx.lock();
+                string current_nickname = get_name_from_socket(client_socket);
+                //remove from active sockets 
+                room = get_room(client_socket);
+                if(rooms[room-1].first == client_socket){
+                    rooms[room-1].first == 0;
+                    other = rooms[room-1].second;
+                }
+                else{
+                    rooms[room-1].second == 0;
+                    other = rooms[room-1].first;
+                }
+                active_sockets.erase(remove(active_sockets.begin(),active_sockets.end(),client_socket),active_sockets.end());
+                stringstream message;
+        		message << "6;" << get_name_from_socket(client_socket);
+                mtx.unlock();
+                send_message(client_socket,"5");                
+                send_message(other,message.str());
+                break;
         	}    
-	    case '8'://Broadcast to all (admin only) (receive 8;<msg>)
-		{
-			mtx.lock();
-		//	admin_send_all_message(received_data);
-			mtx.unlock();
-       			break;
-		}
+    	    case '8'://Broadcast to all (admin only) (receive 8;<msg>)
+		    {
+			    mtx.lock();
+		    	admin_send_all_message(split_str[1]);
+			    mtx.unlock();
+    			break;
+		    }
 	}
 }
